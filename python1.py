4d56207f8176650dbcecba1f9315765301403edc
Sikandar Ali <sikandarwithcode@gmail.com>
2026-02-27 13:36:42 +0500

    First commit

---
+++ python1.py
@@ -0,0 +1,87 @@
+#1
+
+print("Twinkle, twinkle, little star, \n \t How I wonder what you are!\n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \n Twinkle, twinkle, little star,\n \t How I wonder what you are!")
+
+# 2
+import sys
+
+print("Python version", sys.version)
+
+
+#3
+
+import datetime
+
+now=datetime.datetime.now()
+print(now.time())
+print(now.date())
+
+
+#4
+
+from math import pi
+
+rds = float(input("Enter the radius of circle :"))
+area = pi*rds*rds
+print('The area is according to your radius : ',area)
+
+
+#5
+
+fstNm= input('Enter your first Name:')
+lstNm= input('Enter your last Name:')
+
+print(fstNm.capitalize(), lstNm.capitalize())
+
+
+
+# 6
+
+num1 = int(input('Enter first num: '))
+num2 = int(input('Enter second num: '))
+print('Sum is =',num1+num2)
+
+
+
+# 7
+
+isl=int(input('Enter isl number: '))
+urdu=int(input('Enter urdu number: '))
+english=int(input('Enter English number: '))
+pak=int(input('Enter PakStudies number: '))
+physics=int(input('Enter Physics number: '))
+
+obtained = isl+english+pak+physics+urdu
+
+per=obtained/500*100
+
+print('Totals Marks:',obtained)
+print('Persentage',per)
+
+if per >= 80:
+    print('Grade: A+') 
+
+elif per >= 70:
+    print('Grade: A') 
+elif per >= 60:
+    print('Grade: B') 
+elif per >= 50:
+    print('Grade: C') 
+elif per >= 40:
+    print('Grade: D') 
+elif per >= 33:
+    print('Grade: E') 
+
+else:
+    print('You are fail')
+
+
+
+# 8
+
+num=int(input('Enter you number : '))
+
+if num % 2==0:
+    print(num, 'is an Even number' )
+else:
+    print(num, 'is Odd number' )    
