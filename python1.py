b80b0920e92c8e57f9abd920c3561da4f0a82386
Sikandar Ali <sikandarwithcode@gmail.com>
2026-02-28 08:32:04 +0500

    Updated python1 and added python2

--- python1.py
+++ python1.py
@@ -1,87 +1,89 @@
-#1
+# #1
 
-print("Twinkle, twinkle, little star, \n \t How I wonder what you are!\n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \n Twinkle, twinkle, little star,\n \t How I wonder what you are!")
+# print("Twinkle, twinkle, little star, \n \t How I wonder what you are!\n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \n Twinkle, twinkle, little star,\n \t How I wonder what you are!")
 
-# 2
-import sys
+# # 2
+# import sys
 
-print("Python version", sys.version)
-
-
-#3
-
-import datetime
-
-now=datetime.datetime.now()
-print(now.time())
-print(now.date())
+# print("Python version", sys.version)
 
 
-#4
+# #3
 
-from math import pi
+# import datetime
 
-rds = float(input("Enter the radius of circle :"))
-area = pi*rds*rds
-print('The area is according to your radius : ',area)
-
-
-#5
-
-fstNm= input('Enter your first Name:')
-lstNm= input('Enter your last Name:')
-
-print(fstNm.capitalize(), lstNm.capitalize())
+# now=datetime.datetime.now()
+# print(now.time())
+# print(now.date())
 
 
+# #4
 
-# 6
+# from math import pi
 
-num1 = int(input('Enter first num: '))
-num2 = int(input('Enter second num: '))
-print('Sum is =',num1+num2)
+# rds = float(input("Enter the radius of circle :"))
+# area = pi*rds*rds
+# print('The area is according to your radius : ',area)
+
+
+# #5
+
+# fstNm= input('Enter your first Name:')
+# lstNm= input('Enter your last Name:')
+
+# print(fstNm.capitalize(), lstNm.capitalize())
 
 
 
-# 7
+# # 6
 
-isl=int(input('Enter isl number: '))
-urdu=int(input('Enter urdu number: '))
-english=int(input('Enter English number: '))
-pak=int(input('Enter PakStudies number: '))
-physics=int(input('Enter Physics number: '))
-
-obtained = isl+english+pak+physics+urdu
-
-per=obtained/500*100
-
-print('Totals Marks:',obtained)
-print('Persentage',per)
-
-if per >= 80:
-    print('Grade: A+') 
-
-elif per >= 70:
-    print('Grade: A') 
-elif per >= 60:
-    print('Grade: B') 
-elif per >= 50:
-    print('Grade: C') 
-elif per >= 40:
-    print('Grade: D') 
-elif per >= 33:
-    print('Grade: E') 
-
-else:
-    print('You are fail')
+# num1 = int(input('Enter first num: '))
+# num2 = int(input('Enter second num: '))
+# print('Sum is =',num1+num2)
 
 
 
-# 8
+# # 7
 
-num=int(input('Enter you number : '))
+# isl=int(input('Enter isl number: '))
+# urdu=int(input('Enter urdu number: '))
+# english=int(input('Enter English number: '))
+# pak=int(input('Enter PakStudies number: '))
+# physics=int(input('Enter Physics number: '))
 
-if num % 2==0:
-    print(num, 'is an Even number' )
-else:
-    print(num, 'is Odd number' )    
+# obtained = isl+english+pak+physics+urdu
+
+# per=obtained/500*100
+
+# print('Totals Marks:',obtained)
+# print('Persentage',per)
+
+# if per >= 80:
+#     print('Grade: A+') 
+
+# elif per >= 70:
+#     print('Grade: A') 
+# elif per >= 60:
+#     print('Grade: B') 
+# elif per >= 50:
+#     print('Grade: C') 
+# elif per >= 40:
+#     print('Grade: D') 
+# elif per >= 33:
+#     print('Grade: E') 
+
+# else:
+#     print('You are fail')
+
+
+
+# # 8
+
+# num=int(input('Enter you number : '))
+
+# if num % 2==0:
+#     print(num, 'is an Even number' )
+# else:
+#     print(num, 'is Odd number' )    
+
+
---
+++ python2.py
@@ -0,0 +1,24 @@
+# 1
+
+student=['Hamza','Shan','Javed','Rehman','Talha']
+totalStudent=len(student)
+print(totalStudent)
+
+
+# 2
+num=[10,20,30,40,50]
+sm=sum(num)
+print(sm)
+
+
+#3
+num2=[20,55,666,678,2350,4569,542]
+lrgstNm=max(num2)
+print(lrgstNm)
+
+
+#4
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for num in a:
+    if num < 5:
+        print(num)
